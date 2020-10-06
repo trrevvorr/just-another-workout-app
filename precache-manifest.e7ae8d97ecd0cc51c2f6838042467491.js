2dae707b6c6195ed0eb7bf0a02602f31a1f06c08
Trevor Ross <theboss@amazon.com>
2020-10-05 20:28:38 -0400

    deploy

---
+++ precache-manifest.e7ae8d97ecd0cc51c2f6838042467491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b6f4a97a575439b37f",
+    "url": "/just-another-workout-app/css/app.6a7658a3.css"
+  },
+  {
+    "revision": "b6e0b692aeea10ca4db190d8a20cf95f",
+    "url": "/just-another-workout-app/index.html"
+  },
+  {
+    "revision": "41b6f4a97a575439b37f",
+    "url": "/just-another-workout-app/js/app.73a4780a.js"
+  },
+  {
+    "revision": "e66dafeac5a450f55e89",
+    "url": "/just-another-workout-app/js/chunk-vendors.4223bd5c.js"
+  },
+  {
+    "revision": "a05f6bbb486f516bd9af722a174f7789",
+    "url": "/just-another-workout-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/just-another-workout-app/robots.txt"
+  }
+]);
